fd615db84ef673ec71c1a5df167adfa6eb84194b
Pedro Donoso Aguilera <pedro.donosoa@sansano.usm.cl>
2020-03-30 00:47:51 -0300

    Flutter implemented.

---
+++ auto_chela_flutter/android/app/src/main/kotlin/dev/autochela/auto_chela_flutter/MainActivity.kt
@@ -0,0 +1,12 @@
+package dev.autochela.auto_chela_flutter
+
+import androidx.annotation.NonNull;
+import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugins.GeneratedPluginRegistrant
+
+class MainActivity: FlutterActivity() {
+    override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
+        GeneratedPluginRegistrant.registerWith(flutterEngine);
+    }
+}
